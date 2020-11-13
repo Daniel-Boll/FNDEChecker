7428aeea4f42950150f641ff37824e94cdfa762c
Daniel Boll <danielboll.academico@gmail.com>
2020-11-13 20:57:06 -0300

    Adicionar Procfile

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const requestFromAPI = require("./getInfo");
 const schedule = require("node-schedule");
 
-schedule.scheduleJob({ hour: 20, minute: 43 }, () => requestFromAPI.getInfo());
+schedule.scheduleJob({ hour: 21, minute: 05 }, () => requestFromAPI.getInfo());
