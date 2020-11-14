e25f3079ecdc7ea464e12eb883ad8e941eebc6ac
Daniel Boll <danielboll.academico@gmail.com>
2020-11-13 22:27:50 -0300

    Adding error handlers

--- src/getInfo.js
+++ src/getInfo.js
@@ -8,40 +8,46 @@ const mail = require("./sendMail.js");
 module.exports.getInfo = () => {
   const cpf = process.env.CPF;
   const cpf_url = `https://www.fnde.gov.br/digef/rs/spba/publica/pessoa/1/10/${cpf}`;
-  console.log("Fuck");
-  axios.get(cpf_url).then((response) => {
-    console.log("Reaching here");
-    const currentHash = `https://www.fnde.gov.br/digef/rs/spba/publica/pagamento/${response.data.pessoas[0].hash}`;
-    axios.get(currentHash).then((response) => {
-      // Variables from API
-      const payments =
-        response.data.programas[0].entidades[78680337000184].funcoes[49]
-          .pagamentos;
-      const info = {
-        paymentsSize: payments.length,
-      };
 
-      // Check if it has changed
-      const rawData = fs.readFileSync(
-        path.join(__dirname, "..", "storage", "info.json")
-      );
-      const data = JSON.parse(rawData);
+  axios
+    .get(cpf_url)
+    .then((response) => {
+      console.log("Reaching here");
+      const currentHash = `https://www.fnde.gov.br/digef/rs/spba/publica/pagamento/${response.data.pessoas[0].hash}`;
+      axios
+        .get(currentHash)
+        .then((response) => {
+          // Variables from API
+          const payments =
+            response.data.programas[0].entidades[78680337000184].funcoes[49]
+              .pagamentos;
+          const info = {
+            paymentsSize: payments.length,
+          };
 
-      console.log(`${data.paymentsSize} x ${info.paymentsSize}`);
+          // Check if it has changed
+          const rawData = fs.readFileSync(
+            path.join(__dirname, "..", "storage", "info.json")
+          );
+          const data = JSON.parse(rawData);
 
-      if (data.paymentsSize != info.paymentsSize + 1) {
-        fs.writeFile(
-          path.join(__dirname, "..", "storage", "info.json"),
-          JSON.stringify(info),
-          (e) => console.log(e)
-        );
+          console.log(`${data.paymentsSize} x ${info.paymentsSize}`);
 
-        mail.sendMail(
-          (subject = "Bolsa provavelmente caiu!"),
-          (text =
-            "A API notou uma alteração nos dados, há a chance de a bolsa ter caído. ")
-        );
-      }
-    });
-  });
+          if (data.paymentsSize != info.paymentsSize) {
+            fs.writeFile(
+              path.join(__dirname, "..", "storage", "info.json"),
+              JSON.stringify(info),
+              (e) => console.log(e)
+            );
+
+            mail.sendMail(
+              (subject = "Bolsa provavelmente caiu!"),
+              (text =
+                "A API notou uma alteração nos dados, há a chance de a bolsa ter caído. ")
+            );
+          }
+        })
+        .catch((err) => console.log("Error on HASH_URL"));
+    })
+    .catch((err) => console.log("Error on CPF_URL"));
 };
